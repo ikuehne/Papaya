818ec6de9c1a187ef402e1a8453ee81d65235ca4
Noah Nelson <nelson.noah96@gmail.com>
2015-12-18 20:53:39 -0700

    Added stub protocols for Undirected and Directed graphs. Implementations of Graphs should be clear which case they are implementing.

---
+++ Sources/DirectedGraph.swift
@@ -0,0 +1,11 @@
+/**
+ This file defines a protocol for directed graphs.
+ */
+
+/**
+ A directed graph has the same methods as a general graph, with no
+ requirement that adjacency be symmetric.
+ */
+public protocol DirectedGraph: Graph {
+
+}
